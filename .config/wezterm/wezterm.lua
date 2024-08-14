6657136fdf13162c331bb3a3718fa73d020487fb
Quelor <quelor.jb@gmail.com>
2024-08-14 08:18:20 +0200

    removes ligatures

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,8 @@ return {
 	font_size = 15.0,
 	font = wezterm.font("JetBrains Mono"),
 
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+
 	send_composed_key_when_left_alt_is_pressed = true,
 	send_composed_key_when_right_alt_is_pressed = true,
 
